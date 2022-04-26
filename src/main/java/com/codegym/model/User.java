77df5499c323493aa49df095267abb119a3a7f6b
unknown <luongvu2605@gmail.com>
2022-04-26 10:43:53 +0700

    Bai8 bai tap1

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,91 @@
+package com.codegym.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.*;
+@Component
+public class User implements Validator {
+    @NotEmpty
+    @Size(min = 5,max = 45,message = "Do dai tu 5 den 45 ki tu")
+    private String firstName;
+    private String lastName;
+    @Min(value = 18,message = "Tren 18 tuoi")
+    private int age;
+    @Email
+    private String email;
+    private String phoneNumber;
+    public User() {
+    }
+
+    public User(@NotEmpty @Size(min = 5,max = 45) String firstName, String lastName, @Min(value = 18) int age,@NotBlank String email,String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user=(User) target;
+        String phoneNumber= user.getPhoneNumber();
+        ValidationUtils.rejectIfEmpty(errors,"phoneNumber","phoneNumber.empty");
+        if (phoneNumber.length()>11 || phoneNumber.length()<10){
+            errors.rejectValue("phoneNumber", "phoneNumber.length");
+        }
+        if (!phoneNumber.startsWith("0")){
+            errors.rejectValue("phoneNumber", "phoneNumber.startsWith");
+        }
+        if (!phoneNumber.matches("(^$|[0-9]*$)")){
+            errors.rejectValue("phoneNumber", "phoneNumber.matches");
+        }
+    }
+}
